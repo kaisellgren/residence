82caa75c89220ac42473590cb1343b9ff66293d8
Kai Sellgren <kaisellgren@gmail.com>
2015-12-06 07:14:00 -0800

    Update config.template.js

--- config.template.js
+++ config.template.js
@@ -1,3 +1,15 @@
+const path = require('path')
+
 exports.config = {
-  port: 80
+  server: {
+    port: 80,
+    hostname: '0.0.0.0'
+  },
+  cloud: {
+    projectId: 'residence-1144',
+    keyFilename: path.resolve(__dirname, 'gcloud.json'),
+    datastore: {
+      namespace: 'dev'
+    }
+  }
 }
